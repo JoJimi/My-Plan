db1392b8506238e1324e0c58c9d5f0ff4a88c34c
JoJimi <duddnddnfl@naver.com>
2024-07-01 03:57:43 +0900

    날짜 별 일정 작성

--- app/src/main/java/com/example/myplan/TodoActivity.kt
+++ app/src/main/java/com/example/myplan/TodoActivity.kt
@@ -19,6 +19,7 @@ class TodoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityTodoBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        title = "일정"
 
         binding.calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
             binding.diaryDate.visibility = View.VISIBLE
@@ -29,6 +30,7 @@ class TodoActivity : AppCompatActivity() {
             binding.delBtn.visibility = View.INVISIBLE
             binding.diaryDate.text = String.format("%d / %d / %d", year, month + 1, dayOfMonth)
             binding.TodoEditText.setText("")
+
             checkDay(year, month, dayOfMonth, userID)
         }
 
@@ -55,13 +57,26 @@ class TodoActivity : AppCompatActivity() {
             val fileData = ByteArray(fileInputStream.available())
             fileInputStream.read(fileData)
             fileInputStream.close()
+
             str = String(fileData)
-            binding.TodoEditText.visibility = View.INVISIBLE
-            binding.textView.visibility = View.VISIBLE
-            binding.textView.text = str
-            binding.saveBtn.visibility = View.INVISIBLE
-            binding.modifyBtn.visibility = View.VISIBLE
-            binding.delBtn.visibility = View.VISIBLE
+
+            if (binding.textView.text == "") {  // 일정이 비어있는 경우
+                binding. textView.visibility = View.INVISIBLE
+                binding.modifyBtn.visibility = View.INVISIBLE
+                binding.delBtn.visibility = View.INVISIBLE
+                binding.diaryDate.visibility = View.VISIBLE
+                binding.saveBtn.visibility = View.VISIBLE
+                binding.TodoEditText.visibility = View.VISIBLE
+            }
+            else{   // 일정이 있는 경우
+                binding.TodoEditText.visibility = View.INVISIBLE
+                binding.textView.visibility = View.VISIBLE
+                binding.textView.text = str
+                binding.saveBtn.visibility = View.INVISIBLE
+                binding.modifyBtn.visibility = View.VISIBLE
+                binding.delBtn.visibility = View.VISIBLE
+            }
+
             binding.modifyBtn.setOnClickListener {
                 binding.TodoEditText.visibility = View.VISIBLE
                 binding.textView.visibility = View.INVISIBLE
@@ -79,14 +94,6 @@ class TodoActivity : AppCompatActivity() {
                 binding.TodoEditText.visibility = View.VISIBLE
                 binding.saveBtn.visibility = View.VISIBLE
                 removeDiary(fname)
-            }
-            if (binding.textView.text == null) {
-                binding. textView.visibility = View.INVISIBLE
-                binding.modifyBtn.visibility = View.INVISIBLE
-                binding.delBtn.visibility = View.INVISIBLE
-                binding.diaryDate.visibility = View.VISIBLE
-                binding.saveBtn.visibility = View.VISIBLE
-                binding.TodoEditText.visibility = View.VISIBLE
             }
         } catch (e: Exception) {
             e.printStackTrace()
